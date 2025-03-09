2f153cd6faa76a4bef5bd726d3d9043f28de4c89
Utkarsh Anand <growupanand@gmail.com>
2025-03-09 17:29:07 +0530

    Send Correlation ID

--- app/main.js
+++ app/main.js
@@ -1,8 +1,14 @@
-import net from "net";
+import net from "node:net";
 
 
 const server = net.createServer((connection) => {
     // Handle connection
+    const correlationID = 7;
+    const messageSize = 4;
+    const response = Buffer.alloc(8);
+    response.writeInt32BE(messageSize, 0);
+    response.writeInt32BE(correlationID, 4);
+    connection.write(response);
 });
 
 server.listen(9092, "127.0.0.1");

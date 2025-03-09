ebb9bac5ee7e67c53208ff601532e8f5d6a27419
Utkarsh Anand <growupanand@gmail.com>
2025-03-09 18:13:57 +0530

    Parse Correlation ID

--- app/main.js
+++ app/main.js
@@ -3,12 +3,31 @@ import net from "node:net";
 
 const server = net.createServer((connection) => {
     // Handle connection
-    const correlationID = 7;
-    const messageSize = 4;
-    const response = Buffer.alloc(8);
-    response.writeInt32BE(messageSize, 0);
-    response.writeInt32BE(correlationID, 4);
-    connection.write(response);
+    connection.on("data", (data) => {
+        // 
+        /**
+         * We already know the data ArrayBuffer:
+         * 
+         * 00 00 00 23  // message_size:        35
+         * 00 12        // request_api_key:     18
+         * 00 04        // request_api_version: 4
+         * 6f 7f c6 61  // correlation_id:      1870644833
+         */
+
+        // First get the correlation_id Buffer, which position is 8, 12
+        const correlationBuffer = data.subarray(8, 12);
+        // Now read the correlation_id as a 32-bit unsigned integer
+        const correlationID = correlationBuffer.readUInt32BE(0);
+
+        // Create an response
+        const messageSize = 4;
+        const response = Buffer.alloc(8);
+        response.writeInt32BE(messageSize, 0);
+        response.writeInt32BE(correlationID, 4);
+        connection.write(response);
+    })
 });
 
-server.listen(9092, "127.0.0.1");
+server.listen(9092, "127.0.0.1", () => {
+    console.log("Server listening on port 9092", "127.0.0.1:9092");
+});
